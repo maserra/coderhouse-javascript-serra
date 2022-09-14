83c87ba249f9864ac58852fa88ae50626f4680b0
Marcos Serra <marcosserra95@hotmail.com>
2022-09-14 20:45:18 -0300

    Primera entrega del Proyecto Final

--- js/script.js
+++ js/script.js
@@ -11,10 +11,17 @@ let gasto_total = 0
 let variable = "-"
 let eliminar = 0
 
+//html
+let div_ingresos = document.getElementById("ingresos")
+let div_gastos = document.getElementById("gastos")
+let div_inv = document.getElementById("inv")
+let div_resultado = document.getElementById("resultado")
+
 // 1) Cargar ingreso
 function cargar_ingreso(){
     let nuevo_ingreso = parseInt(prompt("Cargar nuevo ingreso"))
     ingresos.push(nuevo_ingreso)
+    div_ingresos.innerText = f_ingresos("ver")
 }
 
 // 2) Eliminar ingreso
@@ -26,7 +33,8 @@ function eliminar_ingreso(){
     }else{
         alert("Opción no válida. Intente de nuevo.")
         eliminar_ingreso()
-    }   
+    }
+    div_ingresos.innerText = f_ingresos("ver")
 }
 
 // 3) Ver ingresos
@@ -36,7 +44,7 @@ function f_ingresos(variable){
         texto_ingreso = texto_ingreso + "\n" + (i+1) + "): " + ingresos[i]
     }
     if(variable == "ver"){
-        alert(texto_ingreso)
+        return texto_ingreso
     }else{
         texto_ingreso = texto_ingreso + "\n ¿Qué valor desea eliminar (escribir el número de la posición)"
         return texto_ingreso
@@ -47,6 +55,7 @@ function f_ingresos(variable){
 function cargar_gasto(){
     let nuevo_gasto = parseInt(prompt("Cargar nuevo gasto"))
     gastos.push(nuevo_gasto)
+    div_gastos.innerText = f_gastos("ver")
 }
 
 // 5) Eliminar gasto
@@ -59,7 +68,7 @@ function eliminar_gasto(){
         alert("Opción no válida. Intente de nuevo.")
         eliminar_gasto()
     }
-    
+    div_gastos.innerText = f_gastos("ver")
 }
 
 // 6) Ver gastos
@@ -69,7 +78,7 @@ function f_gastos(variable){
         texto_gasto = texto_gasto + "\n" + (i+1) + "): " + gastos[i]
     }
     if(variable == "ver"){
-        alert(texto_gasto)
+        return texto_gasto
     }else{
         texto_gasto = texto_gasto + "\n ¿Qué valor desea eliminar? (escribir el número de la posición)"
         return texto_gasto
@@ -79,11 +88,12 @@ function f_gastos(variable){
 // 7) Cambiar inversión
 function cambiar_inv(){
     inv = parseFloat(prompt("Cargar porcentaje de inversión (Número de 0 a 100)"))/100
+    div_inv.innerText = ver_inv()
 }
 
 // 8) Ver porcentaje de inversión
 function ver_inv(){
-    alert("El porcentaje de inversión es: " + inv*100 + "%")
+    return "El porcentaje de inversión es: " + inv*100 + "%"
 }
 
 // 9) Calcular resultado
@@ -99,46 +109,5 @@ function calcular(){
     resultado = ingreso_total - gasto_total
     inversion = resultado * inv
     resultado = resultado - inversion
-    alert("Resultado \n El ingreso total es: $" + ingreso_total + "\n El gasto total es: $" + gasto_total + "\n El monto a invertir es: $" + inversion + "\n El monto restante es: $" + resultado)
+    div_resultado.innerText = "Resultado \n El ingreso total es: $" + ingreso_total + "\n El gasto total es: $" + gasto_total + "\n El monto a invertir es: $" + inversion + "\n El monto restante es: $" + resultado
 }
-
-// Menú
-let menu = 0
-do{
-    menu = parseInt(prompt("Presupuesto personal. \n Elija la opción: \n 1) Cargar ingreso. \n 2) Eliminar ingreso. \n 3) Ver ingresos. \n 4) Cargar gasto. \n 5) Eliminar gasto. \n 6) Ver gastos. \n 7) Cambiar porcentaje de inversión. \n 8) Ver porcentaje de inversión. \n 9) Calcular resultado. \n 0) Salir."))
-    switch (menu){
-        case 1:
-            cargar_ingreso()
-            break
-        case 2:
-            eliminar_ingreso()
-            break
-        case 3:
-            f_ingresos("ver")
-            break
-        case 4:
-            cargar_gasto()
-            break
-        case 5:
-            eliminar_gasto()
-            break
-        case 6:
-            f_gastos("ver")
-            break
-        case 7:
-            cambiar_inv()
-            break
-        case 8:
-            ver_inv()    
-            break
-        case 9:
-            calcular()
-            break
-        case 0:
-            alert("Chau! nos vemos la próxima.")
-            console.log(0)
-            break
-        default:
-            alert("Opción no válida. Intente de nuevo.")
-    }
-}while(menu != 0)
