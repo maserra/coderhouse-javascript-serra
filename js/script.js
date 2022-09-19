61f96d241f40a8dadd30d3821b5e9024573265e8
Marcos Serra <marcosserra95@hotmail.com>
2022-09-19 19:45:53 -0300

    Desafío - Interactuar con HTML

--- js/script.js
+++ js/script.js
@@ -11,12 +11,24 @@ let gasto_total = 0
 let variable = "-"
 let eliminar = 0
 
-//html
+// Clase
+class item {
+    constructor(monto, tipo, fecha, signo){
+        this.monto = monto;
+        this.tipo = tipo;
+        this.fecha = fecha;
+        this.signo = signo;
+    }
+}
+
+// Html
 let div_ingresos = document.getElementById("ingresos")
 let div_gastos = document.getElementById("gastos")
 let div_inv = document.getElementById("inv")
 let div_resultado = document.getElementById("resultado")
 
+let btn_mostrar_ingreso = document.getElementById("btn_mi")
+let btn_mostrar_gasto = document.getElementById("btn_mg")
 let btn_cargar_ingreso = document.getElementById("btn_ci")
 let btn_eliminar_ingreso = document.getElementById("btn_ei")
 let btn_cargar_gasto = document.getElementById("btn_cg")
@@ -24,17 +36,27 @@ let btn_eliminar_gasto = document.getElementById("btn_eg")
 let btn_cambiar_inv = document.getElementById("btn_inv")
 let btn_calcular_resultado = document.getElementById("btn_cal")
 
-btn_cargar_ingreso.onclick = cargar_ingreso
-btn_eliminar_ingreso.onclick = eliminar_ingreso
-btn_cargar_gasto.onclick = cargar_gasto
-btn_eliminar_gasto.onclick = eliminar_gasto
-btn_cambiar_inv.onclick = cambiar_inv
-btn_calcular_resultado.onclick = calcular
+// Funciones de los botones
+btn_mostrar_ingreso.onclick = () => mostrar_ingreso()
+btn_mostrar_gasto.onclick = () => mostrar_gasto()
+btn_cargar_ingreso.onclick = () => cargar_ingreso()
+btn_eliminar_ingreso.onclick = () => eliminar_ingreso()
+btn_cargar_gasto.onclick = () => cargar_gasto()
+btn_eliminar_gasto.onclick = () => eliminar_gasto()
+btn_cambiar_inv.onclick = () => cambiar_inv()
+btn_calcular_resultado.onclick = () => calcular()
 
 // 1) Cargar ingreso
 function cargar_ingreso(){
-    let nuevo_ingreso = parseInt(prompt("Cargar nuevo ingreso"))
+    let monto = parseInt(document.getElementById("monto-ingreso").value)
+    let fecha = document.getElementById("fecha-ingreso").value
+    let tipo = document.getElementById("tipo-ingreso").value
+    let signo = "ingreso" 
+    let nuevo_ingreso = new item (monto, tipo, fecha, signo)
     ingresos.push(nuevo_ingreso)
+    document.getElementById("tipo-ingreso").value = ""
+    document.getElementById("fecha-ingreso").value = ""
+    document.getElementById("monto-ingreso").value = ""
     div_ingresos.innerText = f_ingresos("ver")
 }
 
@@ -53,9 +75,9 @@ function eliminar_ingreso(){
 
 // 3) Ver ingresos
 function f_ingresos(variable){
-    let texto_ingreso = "Ingresos"
+    let texto_ingreso = ""
     for(i = 0; i<ingresos.length; i++){
-        texto_ingreso = texto_ingreso + "\n" + (i+1) + "): " + ingresos[i]
+        texto_ingreso = texto_ingreso + (i+1) + ") " + ingresos[i].fecha + " -> $" + ingresos[i].monto + "\n"
     }
     if(variable == "ver"){
         return texto_ingreso
@@ -67,8 +89,15 @@ function f_ingresos(variable){
 
 // 4) Cargar gasto
 function cargar_gasto(){
-    let nuevo_gasto = parseInt(prompt("Cargar nuevo gasto"))
+    let monto = parseInt(document.getElementById("monto-gasto").value)
+    let fecha = document.getElementById("fecha-gasto").value
+    let tipo = document.getElementById("tipo-gasto").value
+    let signo = "gasto" 
+    let nuevo_gasto = new item (monto, tipo, fecha, signo)
     gastos.push(nuevo_gasto)
+    document.getElementById("tipo-gasto").value = ""
+    document.getElementById("fecha-gasto").value = ""
+    document.getElementById("monto-gasto").value = ""
     div_gastos.innerText = f_gastos("ver")
 }
 
@@ -87,9 +116,9 @@ function eliminar_gasto(){
 
 // 6) Ver gastos
 function f_gastos(variable){
-    let texto_gasto = "Gastos"
+    let texto_gasto = ""
     for(i = 0; i<gastos.length; i++){
-        texto_gasto = texto_gasto + "\n" + (i+1) + "): " + gastos[i]
+        texto_gasto = texto_gasto + (i+1) + ") " + gastos[i].fecha + " -> $" + gastos[i].monto + "\n"
     }
     if(variable == "ver"){
         return texto_gasto
@@ -115,13 +144,33 @@ function calcular(){
     ingreso_total = 0
     gasto_total = 0
     for(i = 0; i < ingresos.length; i++){
-        ingreso_total = ingreso_total + ingresos[i]
+        ingreso_total = ingreso_total + ingresos[i].monto
     }
     for(i = 0; i < gastos.length; i++){
-        gasto_total = gasto_total + gastos[i]
+        gasto_total = gasto_total + gastos[i].monto
     }
     resultado = ingreso_total - gasto_total
     inversion = resultado * inv
     resultado = resultado - inversion
-    div_resultado.innerText = "Resultado \n El ingreso total es: $" + ingreso_total + "\n El gasto total es: $" + gasto_total + "\n El monto a invertir es: $" + inversion + "\n El monto restante es: $" + resultado
+    div_resultado.innerText = "El ingreso total es: $" + ingreso_total + "\n El gasto total es: $" + gasto_total + "\n El monto a invertir es: $" + inversion + "\n El monto restante es: $" + resultado
 }
+
+// Carga de datos
+let btn_close = document.getElementsByClassName("btn-close")
+let formulario_ingreso = document.getElementsByClassName("carga-ingreso")
+let formulario_gasto = document.getElementsByClassName("carga-gasto")
+
+function mostrar_ingreso(){
+    formulario_ingreso[0].style.display = "block"
+}
+function mostrar_gasto(){
+    formulario_gasto[0].style.display = "block"
+}   
+
+function cerrar(){
+    formulario_ingreso[0].style.display = "none"
+    formulario_gasto[0].style.display = "none"
+}
+
+btn_close[0].onclick = () => cerrar()
+btn_close[1].onclick = () => cerrar()
